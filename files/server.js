01f6bc8eedeadde558e31c2ed0d24289b76a51ab
Lillian Dourlain <lcd64@drexel.edu>
2019-11-18 18:22:43 -0500

    Add two endpoints to test WHO API

---
+++ files/server.js
@@ -0,0 +1,81 @@
+/* Dependencies */
+let express = require("express");
+let request = require("request");
+let bodyParser = require("body-parser"); // for posting form data
+let mysql = require("mysql");
+
+var app = express();
+
+/* Endpoints */
+
+// Only use static files from static folder
+app.use(express.static("./static"));
+
+/* /get-countries
+ * Purpose: Gets list of all countries WHO tracks
+ * Query parameters: None
+ * Notes: Goes very slow, should only be used to update OUR database, every so often.
+ */
+app.get("/get-countries", function (req, res) {
+    let URL = "http://apps.who.int/gho/athena/api/COUNTRY?format=json";
+    request.get(URL, function (error, response, body) {
+        let json = JSON.parse(body);
+        let countries = json.dimension[0].code;
+
+        // Because of the speed of connection to the WHO API, we might not want to output any JSON from this url
+        // Or add a seperate URL to 
+        // Todo: Update to modify database
+        // Remember: Country may already exist in the database.
+        output = {};
+        for (let i = 0; i < countries.length; i++) {
+            let label = countries[i].label; // USA
+            let display = countries[i].display; // United States of America
+            output[label] = display;
+        }
+
+        res.json(output);
+        res.end();
+    });
+
+});
+
+
+/* /get-indicator
+ * Purpose: Gets all information on an "indicator" (disease, statistic, etc...)
+ * Query parameters: indicator (Label for an indicator) EX: WHOSIS_000012
+ * Notes: see notes for /get-countries
+ */
+app.get("/get-indicator", function (req, res) {
+    let indicator = req.query.indicator;
+    let URL = "http://apps.who.int/gho/athena/api/GHO/" + indicator + "?format=json&profile=simple";
+    request.get(URL, function (error, response, body) {
+        let json = JSON.parse(body);
+        let dataPoints = json.fact;
+
+        // Again, see /get-countries
+        // It will also be MUCH easier to sort this data in a database.
+        // Todo: fix to work with database
+        output = {
+            "label": dataPoints[0].dim.GHO, // Life expectancy ...
+            "results": []
+        };
+
+        for (let i = 0; i < dataPoints.length; i++) {
+            let data = {}
+            data.country = dataPoints[i].dim.COUNTRY; // Argentina
+            data.year = dataPoints[i].dim.YEAR; // 2006...
+            data.sex = dataPoints[i].dim.SEX; // Female
+            data.region = dataPoints[i].dim.REGION; // Americas
+            output.results.push(data);
+        }
+
+        res.json(output);
+        res.end();
+    });
+});
+
+/* Express Listen */
+
+app.listen(8080, function () {
+    console.log("Server listening on http://localhost:8080...")
+});
