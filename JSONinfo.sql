071b1de18f8fa954b3c7e53c05f13e8949b10632
Brandon Gordon <brandonag1997@gmail.com>
2019-12-06 16:45:39 -0500

    SQL qs

--- JSONinfo.sql
+++ JSONinfo.sql
@@ -1,8 +1,9 @@
-SELECT DISTINCT(I.IndicatorName), V.CountryShort, V.Sex
+SELECT DISTINCT(I.IndicatorName), V.Country, V.Sex
 FROM Indicator AS I
 INNER JOIN IndicatorValue AS V
 ON V.IndicatorShort = I.IndicatorShort
-GROUP BY V.CountryShort, V.Sex;
+GROUP BY V.Country, V.Sex
+LIMIT 5;
 
 SELECT INDICATOR_NAME TEXT,
 YEAR DATE,
@@ -18,5 +19,28 @@ INSERT INTO Indicator (IndicatorShort,IndicatorName) VALUES ();
 INSERT INTO IndicatorValue (IndicatorID,Year,Value,Sex,CountryShort,RegionShort,IndicatorShort) VALUES ()
 
 
-SELECT DISTINCT(I.IndicatorName) FROM Indicator AS I
+SELECT * FROM COUNTRY
+LIMIT 5;
+
+SELECT JSON_OBJECT(
+  DisplayName,'test'
+)
+FROM COUNTRY
+LIMIT 5;
+
+SELECT JSON_OBJECT(
+  DisplayName,json_array(
+    (SELECT
+      JSON_OBJECT("fillKey","HIGH","numberOfThings",V.Value)
+
+    FROM IndicatorValue AS V
+    INNER JOIN COUNTRY AS C
+    ON V.Country = C.DisplayName))
+  )
+FROM COUNTRY AS C
+LIMIT 5;
+
+
+SELECT * --V.Value
+FROM IndicatorValue AS V
 LIMIT 5;
